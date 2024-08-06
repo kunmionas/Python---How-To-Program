7842945f5091bafe04999df50e6ac64b345b63c1
kunmionas <kunmionas02@gmail.com>
2024-08-06 12:29:07 +0100

    added files

--- Python - How to Program/5.py
+++ Python - How to Program/5.py
@@ -22,7 +22,7 @@ g) t
 h) t
 i) f
 j) t
-
+'''
 
 
 # 5.5 Sieve of Eratosthenes
@@ -52,25 +52,80 @@ def sieve_of_eratosthenes(gap):
 
 user_gap = int(input("Enter a number: "))
 print(f"There are {len(sieve_of_eratosthenes(user_gap))} prime numbers between 0 and {user_gap}")
-'''
+
 
 # 5.6 Bubble Sort
+
 def bubbleSort(list1):
 
-    item1 = 0
-    item2 = 0
+    n = len(list1)
 
-    for unit in list1:
+    # Traverse through all array elements
+    for i in range(n):
+        swapped = False
 
-        for comp in list1:
-            if unit > comp:
-                comp = unit
+        # Last i elements are already in place
+        for j in range(0, n - i - 1):
 
-    return list1
+            # Traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if list1[j] > list1[j + 1]:
+                list1[j], list1[j + 1] = list1[j + 1], list1[j]
+                swapped = True
+        if not swapped:
+            break
 
 
-lista = [3, 2, 1]
-print(bubbleSort(lista))
+# Driver code to test above
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+bubbleSort(arr)
+
+print(f"Sorted array: {arr}")
+
+
+
+# 5.7 Binary Search
+
+def binarysearch(search_key, list2):
+
+    if len(list2) == 1 and list2[0] != search_key:
+        return "\nItem is not in list"
+
+    mid_index = len(list2)//2
+    mid_value = list2[mid_index]
+
+    if mid_value == search_key:
+        return mid_index + 1
+
+    elif mid_value > search_key:
+        return binarysearch(search_key, list2[:mid_index])
+
+    else:
+        result = binarysearch(search_key, list2[mid_index + 1:])
+        if result == "\nItem is not in list":
+            return result
+        return mid_index + 1 + result
+
+
+# Example call:
+import random
+example_list = sorted([random.randrange(1, 1000) for _i in range(20)])
+print(example_list)
+
+key = int(input("What number would you like to search this list for? "))
+
+result = binarysearch(key, example_list)
+
+if type(result) == int:
+    print(f"\n{key} was found in the list at position {result}")
+
+else:
+    print(result)
+
+
+
 
 
 
